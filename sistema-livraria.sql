ee02263843e979fb90825edc9a3ca81e3598f802
EduardoCavDias <eduardocavalheirodias4@gmail.com>
2025-05-21 08:25:50 -0300

    Update sistema-livraria.sql

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -11,3 +11,18 @@ CREATE TABLE Compras (
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
 
+-- Inserindo dados na tabela Clientes
+INSERT INTO Clientes (nomeCliente, emailCliente) VALUES 
+('Alice Santos', 'alice@email.com'),
+('Bruno Lima', 'bruno@email.com'),
+('Carla Souza', 'carla@email.com');
+
+-- Inserindo dados na tabela Compras
+INSERT INTO Compras (ClienteID, NomeLivro) VALUES 
+(1, 'O Senhor dos Anéis'),
+(2, '1984'),
+(3, 'Dom Quixote'),
+(1, 'Harry Potter'),
+(2, 'O Pequeno Príncipe');
+
+SELECT * FROM Clientes;
